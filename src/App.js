6a2b8ab5bccd266c2bdd7bde08d758f25d74a424
Adam Perez <Adam@Domoniques-MBP.hsd1.fl.comcast.net>
2023-10-30 19:29:01 -0400

    working on dashboard

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       {/* <Login/> */}
-      <DefaultLayout>//Dashboard</DefaultLayout>
+      <DefaultLayout><Dashboard/></DefaultLayout>
     </div>
   );
 }
