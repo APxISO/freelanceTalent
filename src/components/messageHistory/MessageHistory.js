80ce278a6c8216ce009cef6be075f9824397126f
Adam Perez <Adam@Domoniques-MBP.hsd1.fl.comcast.net>
2023-11-10 06:05:35 -0500

    created message ticket history component

--- src/components/messageHistory/MessageHistory.js
+++ src/components/messageHistory/MessageHistory.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './messageHistory.css';
 
 export const MessageHistory = ({msg}) => {
 
 console.log(msg)
     return msg.map((row,i)=>
-    <div key={i} className='message_history'>
-        <div className='send'>
+    <div key={i} className='message_history mt-3'>
+        <div className='send font-weight-bold text-secondary' >
         <div className='sender'>{row.messageBy}</div>
         <div className='date'>{row.date}</div>
     </div>
@@ -17,4 +18,5 @@ console.log(msg)
 
 MessageHistory.propTypes = {
     msg: PropTypes.array.isRequired,
+    
 }
--- src/pages/ticket/Ticket.js
+++ src/pages/ticket/Ticket.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { Container, Row, Col, Button,  } from "react-bootstrap";
 import { PageBreadcrumbs } from '../../components/breadcrumbs/Breadcrumbs';
 import tickets from '../../assets/data/dummy_data.json';
 import { MessageHistory } from '../../components/messageHistory/MessageHistory';
+import { UpdateTicket } from '../../components/updateTicket/UpdateTicket';
+
 
 const ticket = tickets[0];
 
 
 export const Ticket = () => {
-  
+  const [message, setMessage] = useState('')
+
+  const handleOnChange = (e) =>{
+    setMessage(e.target.value);
+  }
+
+  const handleOnSubmit = () =>{
+    alert('Form submitted!')
+  }
   
     return (
     <Container>
@@ -20,16 +30,23 @@ export const Ticket = () => {
         <Row>
             <Col className='text-weight-bolder text-secondary'>
             <div className='subject'>Subject: {ticket.subject}</div>
-            <div className='date'>Date: {ticket.addedAt}</div>
+            <div className='date'>Ticket Opened: {ticket.addedAt}</div>
             <div className='status'>Status: {ticket.status}</div>
             </Col>
             <Col className='text-right'>
             <Button variant='outline-info' >Close Ticket</Button>
             </Col>
         </Row>
-        <Row>
+        <Row className='mt-4'>
             <Col>
                 <MessageHistory msg={ticket.history}/>
+            </Col>
+        </Row>
+        <hr/>
+        <Row className='mt-4'>
+            <Col>
+            <UpdateTicket msg={message} handleOnChange={handleOnChange} handleOnSubmit={handleOnSubmit} />
+
             </Col>
         </Row>
     </Container>
